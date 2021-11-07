ccb201fa1ff2524b717a67b865501dd42f57656f
Max Ong Zong Bao <max@codesurance.com>
2021-11-07 14:27:12 +0800

    refactor: Changes to the profile url variable to ask for the user to use their own instead of my own

--- proxycurl_quickstart.py
+++ proxycurl_quickstart.py
@@ -9,7 +9,7 @@ proxycurl_api_key = os.getenv('PROXYCURL')  # The API key from Proxycurl
 # Proxycurl API endpoint called "LinkedIn Person Profile".
 # For more details of the API - https://nubela.co/proxycurl/docs#people-api-linkedin-person-profile-endpoint
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-profile_url = 'https://www.linkedin.com/in/maxongzb/'   # The LinkedIn profile url you want to get more information with
+profile_url = 'your_linkedin_profile_url'   # The LinkedIn profile url you want to get more information with
 
 # The header & parameter configuration of the API request
 headers = {'Authorization': 'Bearer ' + proxycurl_api_key}

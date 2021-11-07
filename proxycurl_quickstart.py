7c8e8db6ae30d3ad2dc45b93022d595d522a5bcc
Max Ong Zong Bao <max@codesurance.com>
2021-11-07 13:21:03 +0800

    docs: Updating the comments of the proxycurl_quickstart.py to make it easier to understand.

--- proxycurl_quickstart.py
+++ proxycurl_quickstart.py
@@ -1,19 +1,22 @@
+# importing of python libraries
 import os
 import requests
 from dotenv import load_dotenv
-load_dotenv()
 
-proxycurl_api_key = os.getenv('PROXYCURL')
+load_dotenv()  # Loads the .env configuration as part of your environment variables
+proxycurl_api_key = os.getenv('PROXYCURL')  # The API key from Proxycurl
+
+# Proxycurl API endpoint called "LinkedIn Person Profile".
+# For more details of the API - https://nubela.co/proxycurl/docs#people-api-linkedin-person-profile-endpoint
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-profile_url = 'https://www.linkedin.com/in/maxongzb/'
+profile_url = 'https://www.linkedin.com/in/maxongzb/'   # The LinkedIn profile url you want to get more information with
 
-header_dic = {'Authorization': 'Bearer ' + proxycurl_api_key}
-params = {
-    'url': profile_url,
-    'use_cache': 'if-present',
-}
+# The header & parameter configuration of the API request
+headers = {'Authorization': 'Bearer ' + proxycurl_api_key}
+params = {'url': profile_url, 'use_cache': 'if-present'}
 
-response = requests.get(api_endpoint, params=params, headers=header_dic)
+# The calling the API using request and save it into response
+response = requests.get(api_endpoint, params=params, headers=headers)
 
-data = response.json()
+data = response.json()  # Saves the API response as JSON into "data" variable
 print(data)

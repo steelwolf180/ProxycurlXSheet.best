6dfafd3969c9c6464ca1c3bfa2372fb193b8aef3
Max Ong Zong Bao <max@codesurance.com>
2021-11-07 15:14:56 +0800

    feat: Added the display_google_sheet_data function. Changed variable name from sheet_best_api_key to sheet_best_api_url. Code cleanup to make it readable.

--- final.py
+++ final.py
@@ -4,22 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 proxycurl_api_key = os.getenv('PROXYCURL')
-sheet_best_api_key = os.getenv('SHEET_BEST')
+sheet_best_api_url = os.getenv('SHEET_BEST_URL')
 
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
 profile_url = 'https://www.linkedin.com/in/maxongzb/'
 
 
-def get_linkedin_data(url):
+def get_linkedin_profile_data(url):
     header_dic = {'Authorization': 'Bearer ' + proxycurl_api_key}
     params = {
         'url': url,
         'use_cache': 'if-present',
     }
 
-    response = requests.get(api_endpoint,
-                            params=params,
-                            headers=header_dic)
+    response = requests.get(api_endpoint, params=params, headers=header_dic)
 
     data = response.json()
     print(data)
@@ -27,4 +25,10 @@ def get_linkedin_data(url):
     return data
 
 
-get_linkedin_data(profile_url)
+def display_google_sheet_data():
+    response = requests.get(sheet_best_api_url)  # Saves the API response as JSON into "data" variable
+
+    data = response.json()
+    print(data)
+
+    return data

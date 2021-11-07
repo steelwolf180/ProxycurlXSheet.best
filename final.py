a7727296bd82d7096d1fa9e0432c89649cf30243
Max Ong Zong Bao <max@codesurance.com>
2021-11-07 12:25:46 +0800

    refactor: Changes from company to individual LinkedIn profile as this burns a lot for credits now your just left with only burning 1 credit per call.

--- final.py
+++ final.py
@@ -6,20 +6,25 @@ load_dotenv()
 proxycurl_api_key = os.getenv('PROXYCURL')
 sheet_best_api_key = os.getenv('SHEET_BEST')
 
-api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/'
-company_url = 'https://www.linkedin.com/company/streamlit/'
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+profile_url = 'https://www.linkedin.com/in/maxongzb/'
 
 
 def get_linkedin_data(url):
     header_dic = {'Authorization': 'Bearer ' + proxycurl_api_key}
     params = {
-        'employment_status': 'current',
         'url': url,
+        'use_cache': 'if-present',
     }
+
     response = requests.get(api_endpoint,
                             params=params,
                             headers=header_dic)
-    print(response.json())
 
-    return response.json()
+    data = response.json()
+    print(data)
 
+    return data
+
+
+get_linkedin_data(profile_url)
--- proxycurl_quickstart.py
+++ proxycurl_quickstart.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 proxycurl_api_key = os.getenv('PROXYCURL')
-api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/'
-company_url = 'https://www.linkedin.com/company/streamlit/'
+api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+profile_url = 'https://www.linkedin.com/in/maxongzb/'
 
 header_dic = {'Authorization': 'Bearer ' + proxycurl_api_key}
 params = {
-    'employment_status': 'current',
-    'url': company_url,
+    'url': profile_url,
+    'use_cache': 'if-present',
 }
 
 response = requests.get(api_endpoint, params=params, headers=header_dic)

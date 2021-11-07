43bd31f82f9932796931ea68598af5c8fd6a1c37
Max Ong Zong Bao <max@codesurance.com>
2021-11-07 14:58:52 +0800

    refactor: changed the data variable to use response variable. Once the API is called it saves the response data as json datatype and display it.

--- sheet_best_quickstart.py
+++ sheet_best_quickstart.py
@@ -6,5 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads the .env configuration as part of your environment variables
 sheet_best_connection_URL = os.getenv('SHEET_BEST_URL')  # The API key from Proxycurl
 
-data = requests.get(sheet_best_connection_URL)  # Saves the API response as JSON into "data" variable
-print(data.json())
+response = requests.get(sheet_best_connection_URL)  # Saves the API response as JSON into "data" variable
+data = response.json()
+print(data)
